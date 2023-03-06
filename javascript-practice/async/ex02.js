ccce39b6ad7b996c3bf497fc6efb3463c4a69faf
memory98 <asdlkjh252567@gmail.com>
2023-03-06 15:53:22 +0900

    es6 #3

---
+++ javascript-practice/async/ex02.js
@@ -0,0 +1,27 @@
+
+const myAsyncFn02 = function(param) {
+    return new Promise(function(resolve,reject) {
+        //
+        // 비동기 코드
+        // 파일 시스템 접근(file io), 네트워크 통신, SQL to DB, setTimeout
+        //
+        setTimeout(function() {
+            if(param ==='data') {
+                resolve('ok');
+            } else {
+                reject(new Error('fail'));
+            }
+        },2000);
+    });
+}
+
+// test01 : success
+myAsyncFn02('data').then(function(result){
+    console.log(result);
+});
+
+// test01 : fail
+myAsyncFn02('').catch(function(error) {
+    console.error(error);
+}); 
+console.log('wait........')
